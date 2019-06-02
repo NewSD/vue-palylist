2d7aeee21325a181fb820ba6e0e45d04ae76a7a1
18300603955 <oceaniu@163.com>
2019-06-02 22:42:23 +0800

    vue-playlist

---
+++ src/components/Users.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="users">
+      <ul>
+        <li v-for="user in users">
+          {{user}}
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Users",
+      data(){
+          return{
+            users:['H','B','E']
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
