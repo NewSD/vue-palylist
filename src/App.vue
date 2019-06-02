9d7c4495a231e7d18155d65c0c79e3f3d3700511
18300603955 <oceaniu@163.com>
2019-06-02 23:13:20 +0800

    Header Footer

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,44 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <!--<HelloWorld/>-->
-    <h1>{{title}}</h1>
+    <app-header></app-header>
     <users></users>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import Users from './components/Users'
-export default {
-  name: 'App',
-  data(){
-    return{
-      title:"这是我的第一个Vue脚手架项目"
+  import HelloWorld from './components/HelloWorld'
+  import Users from './components/Users'
+  import Header from './components/Header'
+  import Footer from './components/Footer'
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        title: "这是我的第一个Vue脚手架项目"
+      }
+    },
+    components: {
+      HelloWorld,
+      "users": Users,
+      "app-header": Header,
+      "app-footer": Footer
     }
-  },
-  components: {
-    HelloWorld,
-    "users":Users
   }
-}
 </script>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1{
-  color: purple;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1 {
+    color: purple;
+  }
 </style>
